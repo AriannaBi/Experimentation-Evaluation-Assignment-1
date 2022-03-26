3df412a57b2597da7ea6fa1a064598dcee8f5d0d
Onciu Petre <onciurpetre@gmail.com>
2022-03-26 12:46:47 +0100

    Aggiunto il generatore random di stringhe

--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
 import java.util.*;
 
 
-
 public class Main{
 
-    public static void main(String[] args) {
-        BubbleSortWhileNeeded bs = new BubbleSortWhileNeeded();
-        // final ArrayList<T> T = new ArrayList<T>();
-        // bs.sort();
+    // Genera una stringa a random usando i caratteri da "a" a "z" di un certo size
+    public static String generatedString() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 6;
+        Random random = new Random();
+    
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+          .limit(targetStringLength)
+          .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+          .toString();
+    
+        return generatedString;
+    }
 
-        
+    public static void main(String[] args) {
+        String s = generatedString();
+        System.out.println(s);
     }
     
 }
--- Sorter.java
+++ Sorter.java
@@ -1,19 +1,19 @@
 import java.util.*;
+
 interface Sorter<T extends Comparable<T>> {
 
 	void sort(T[] items);
-
+	
 	public static void main(String[] args) {
-        BubbleSortWhileNeeded bs = new BubbleSortWhileNeeded();
-        Integer[] arr = new Integer[20];
+		BubbleSortWhileNeeded bs = new BubbleSortWhileNeeded();
+		Integer[] arr = new Integer[20];
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = 4;
 		}
 		System.out.println(arr);
-        bs.sort(arr);
+		bs.sort(arr);
 		System.out.println(arr);
 
-        
-    }
+	}
 
 }
