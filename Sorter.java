3821dc4338a312a2284314be8d9dbc4f0453baa3
Onciu Petre <onciurpetre@gmail.com>
2022-03-28 17:50:30 +0200

    fixed

--- Sorter.java
+++ Sorter.java
@@ -4,11 +4,13 @@ import DataFile.DataFile;
 interface Sorter<T extends Comparable<T>> {
 
 	void sort(T[] items);
-	
+
+	int iterations = 500;
+	BubbleSortWhileNeeded whileNeeded = new BubbleSortWhileNeeded();
+	BubbleSortUntilNoChange untilNoChange = new BubbleSortUntilNoChange();
+	BubbleSortPassPerItem passPerItem = new BubbleSortPassPerItem();
+
 	public static void main(String[] args) {
-		BubbleSortWhileNeeded whileNeeded = new BubbleSortWhileNeeded();
-		BubbleSortUntilNoChange untilNoChange = new BubbleSortUntilNoChange();
-		BubbleSortPassPerItem passPerItem = new BubbleSortPassPerItem();
 		Integer[] a = DataFile.A;
 		Integer[] b = DataFile.B;
 		Integer[] c = DataFile.C;
@@ -21,7 +23,7 @@ interface Sorter<T extends Comparable<T>> {
 		String[] j = DataFile.J;
 		String[] k = DataFile.K;
 		String[] l = DataFile.L;
-		Object[] all = {a, b, c, d, e, f, g, h, i, j, k, l};
+		Object[] all = { a, b, c, d, e, f, g, h, i, j, k, l };
 		float[] timeA1 = new float[12];
 		float[] timeA2 = new float[12];
 		float[] timeA3 = new float[12];
@@ -29,32 +31,37 @@ interface Sorter<T extends Comparable<T>> {
 		// 500 iterations
 		// Warm up 1 cycle
 
-		
 	}
 
-	public static void testWhileNeeded(Object[][] all, float[] timeA1){
-		long start = System.nanoTime();
+	public static void testWhileNeeded(Object[][] all, float[] timeA1) {
+		for (Object o : all) {
+			// warm up
+			for (int i = 0; i < iterations; i++) {
+				long start = System.nanoTime();
+				whileNeeded.sort(o);
+				long end = System.nanoTime();
+				long result = end - start;
 
-		long end = System.nanoTime();
-        long result = end - start;
+			}
+		}
+
 		return;
 	}
 
-	public static void testUntilNoChange(float[] timeA2){
+	public static void testUntilNoChange(float[] timeA2) {
 		long start = System.nanoTime();
 
 		long end = System.nanoTime();
-        long result = end - start;
+		long result = end - start;
 		return;
 	}
 
-	public static void testPassPerItem(float[] timeA3){
+	public static void testPassPerItem(float[] timeA3) {
 		long start = System.nanoTime();
 
 		long end = System.nanoTime();
-        long result = end - start;
+		long result = end - start;
 		return;
 	}
-
 
 }
